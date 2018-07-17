f05bd116c80e1abe95458ba8318771fcdbf68408
Rodolfo Milanez <rodolfomilanez@gmail.com>
2018-07-17 17:14:44 +0100

    Add progressive delay

--- src/GuzzleHandler.php
+++ src/GuzzleHandler.php
@@ -34,7 +34,7 @@ class GuzzleHandler
     {
         $handler_stack = \GuzzleHttp\HandlerStack::create();
 
-        $handler_stack->push(\GuzzleHttp\Middleware::retry($this->retryHandler($this->logger)));
+        $handler_stack->push(\GuzzleHttp\Middleware::retry($this->retryHandler($this->logger), $this->retryDelay()));
 
         return new \GuzzleHttp\Client(['handler' => $handler_stack]);
 
@@ -86,5 +86,15 @@ class GuzzleHandler
     function isConnectError(RequestException $exception = null)
     {
         return $exception instanceof ConnectException;
+    }
+
+    /**
+     * delay 1s 2s 3s 4s 5s
+     */
+    public function retryDelay()
+    {
+        return function ($numberOfRetries) {
+           return 1000 * $numberOfRetries;
+        };
     }
 }
